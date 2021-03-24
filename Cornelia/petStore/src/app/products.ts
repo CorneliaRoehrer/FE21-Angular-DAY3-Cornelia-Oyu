96b3c05cb7db01351c38cf3ea631039676cdcd5d
CorneliaRoehrer <corne.lia.roehrer@gmail.com>
2021-03-24 14:25:39 +0100

    add product details and service cart

---
+++ Cornelia/petStore/src/app/products.ts
@@ -0,0 +1,18 @@
+export const products = [
+
+    {
+        name: 'Cat Litter',
+        price: 5,
+        description: 'A large bag of cat litter to hold your cats excrements'
+    },
+    {
+        name: 'Poop bags',
+        price: 2,
+        description: 'A roll of bags to take with you on walks to pick up your dogs excrements'
+    },
+    {
+        name: 'Bird Seed',
+        price: 6,
+        description: 'A big bag of bird seed to attrackt any and all birds, so your cat can have some fun and exercise'
+    }
+    ];
